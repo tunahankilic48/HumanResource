f6625c603f1eb6be85317d222c180a661fc9d2f6
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-29 10:06:36 +0000

    Updated District.cs

--- HumanResource.Domain/Entities/District.cs
+++ HumanResource.Domain/Entities/District.cs
@@ -11,20 +11,17 @@ namespace HumanResource.Domain.Entities
 
         public int Id { get; set; }
         public string Name { get; set; }
-
-        //nav prop
         public int CityId { get; set; }
-        public City City { get; set; }
-
-
-        public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
 
         public int StatuId { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
         public Statu Statu { get; set; }
+
+        // Navigation Property
+        public List<Address> Addresses { get; set; }
+        public City City { get; set; }
     }
     
 }
