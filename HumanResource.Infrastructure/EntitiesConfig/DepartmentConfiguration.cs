82285cbc0270463b2c993a4fef95566afcb6770c
user <user@LAPTOP-5V57UFUH>
2023-04-29 14:42:47 +0300

    Departmant Configuratin was created.

---
+++ HumanResource.Infrastructure/EntitiesConfig/DepartmentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using HumanResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanResource.Infrastructure.EntitiesConfig
+{
+	internal class DepartmentConfiguration : BaseEntitiyConfig<Department>
+	{
+		public override void Configure(EntityTypeBuilder<Department> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id)
+				.HasColumnOrder(1);
+
+			builder.Property(x => x.Name)
+				.IsRequired(true)
+				.IsUnicode(true)
+				.HasMaxLength(255)
+				.HasColumnOrder(2);
+
+			builder.Property(x => x.StatuId)
+				.IsRequired(true)
+				.HasColumnOrder(3);
+
+			base.Configure(builder);
+		}
+	}
+}
