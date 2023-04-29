c319e826c1ee385689f39d92787f643933cb12df
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-29 09:15:22 +0000

    Updated Department.cs

--- HumanResource.Domain/Entities/Department.cs
+++ HumanResource.Domain/Entities/Department.cs
@@ -10,5 +10,8 @@ namespace HumanResource.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //Navigation Property
+        public List<AppUser> Users { get; set; }
     }
 }
