29a1875e7911814f2face1e76aab1de594f3bc56
user <user@LAPTOP-5V57UFUH>
2023-04-29 11:34:15 +0300

    Statu was created.

---
+++ HumanResource.Domain/Entities/Statu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Domain.Entities
+{
+	public class Statu
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime? ModifiedDate { get; set; }
+		public DateTime? DeletedDate { get; set; }
+
+		public List<AppUser> AppUsers { get; set; }
+		public List<LeaveType> LeaveTypes { get; set; }
+		public List<Leave> Leaves { get; set; }
+		public List<Advance> Advances { get; set; }
+		public List<Department> Departments { get; set; }
+	}
+}
