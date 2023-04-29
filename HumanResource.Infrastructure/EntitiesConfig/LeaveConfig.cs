ab8f231d1617983e38337b7e1fd67cd2039cb54c
ahmetislerr <ahmetislertr@gmail.com>
2023-04-29 14:31:39 +0300

    LeaveConfing has created.

---
+++ HumanResource.Infrastructure/EntitiesConfig/LeaveConfig.cs
@@ -0,0 +1,48 @@
+﻿using HumanResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Infrastructure.EntitiesConfig
+{
+	internal class LeaveConfig : BaseEntityConfig<Leave>
+	{
+		public override void Configure(EntityTypeBuilder<Leave> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id)
+				.HasColumnOrder(1);
+
+			builder.Property(x => x.StartDate)
+					.IsRequired(true)
+					.HasColumnType("date")
+					.HasColumnOrder(2);
+
+
+			builder.Property(x => x.EndDate)
+					.IsRequired(true)
+					.HasColumnType("date")
+					.HasColumnOrder(3);
+
+
+			builder.Property(x => x.ReturnDate)
+					.IsRequired(true)
+					.HasColumnType("date")
+					.HasColumnOrder(4);
+
+			builder.Property(x => x.LeaveTypeId)
+					.IsRequired(true)
+					.HasColumnOrder(5);
+
+			builder.HasOne(x => x.LeaveType)
+			   .WithMany(x => x.Leaves)
+			   .HasForeignKey(x => x.LeaveTypeId)
+							   .OnDelete(DeleteBehavior.Restrict);
+
+		}
+	}
+}
