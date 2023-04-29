526f4728eb2a8cafc418d025dd9e9dce51099eeb
ahmetmanco <ahmetmanco07@gmail.com>
2023-04-29 12:45:10 +0300

    Address created

---
+++ HumanResource.Domain/entities/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Domain.Entities
+{
+    public class Address : IBaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PostCode { get; set; }
+        public int CityId { get; set; }
+        public int DistrictId { get; set; }
+
+        //navigation
+        public City City { get; set; }
+        public District District { get; set; }
+    }
+}
