456166311098776878f9f271b18ad0ac06d6a04d
ahmetmanco <ahmetmanco07@gmail.com>
2023-04-29 14:35:26 +0300

    AppUserConfig and BaseEntityConfig craeted

---
+++ HumanResource.Infrastructure/EntitiesConfig/AppUserConfig.cs
@@ -0,0 +1,22 @@
+﻿using HumanResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Infrastructure.EntitiesConfig
+{
+    public class AppUserConfig : BaseEntityConfig<AppUser>
+    {
+        public override void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FirstName).IsRequired(true).HasMaxLength(30);
+            builder.Property(x => x.LastName).IsRequired(true).HasMaxLength(50);
+
+            base.Configure(builder);
+        }
+    }
+}
---
+++ HumanResource.Infrastructure/EntitiesConfig/BaseEntityConfig.cs
@@ -0,0 +1,22 @@
+﻿using HumanResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Infrastructure.EntitiesConfig
+{
+    public abstract class BaseEntityConfig<T> : IEntityTypeConfiguration<T> where T : class, IBaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.CreatedDate).IsRequired(true);
+            builder.Property(x => x.DeletedDate).IsRequired(false);
+            builder.Property(x => x.ModifiedDate).IsRequired(false);
+            builder.Property(x => x.Statu).IsRequired(true);
+        }
+    }
+}
