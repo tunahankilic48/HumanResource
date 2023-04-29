a161f5d028eef6d6668eaf6aab9695ddad2ed5a2
Tunahan Kilic <tunahankilic942@gmail.com>
2023-04-29 14:48:54 +0300

    City and Address Config were created.

---
+++ HumanResource.Infrastructure/EntitiesConfig/AddressConfig.cs
@@ -0,0 +1,46 @@
+﻿using HumanResource.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HumanResource.Infrastructure.EntitiesConfig
+{
+    internal class AddressConfig : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnOrder(1);
+
+            builder.Property(x => x.Name)
+                .IsRequired(true)
+                .IsUnicode(true)
+                .HasColumnType("NVARCHAR(50)")
+                .HasColumnOrder(2);
+
+            builder.Property(x => x.Description)
+                .IsRequired(true)
+                .IsUnicode(true)
+                .HasColumnType("NVARCHAR(MAX)")
+                .HasColumnOrder(3);
+
+            builder.Property(x => x.PostCode)
+                .IsRequired(false)
+                .HasColumnOrder(4);
+
+            builder.Property(x => x.DistrictId)
+                .IsRequired(true)
+                .HasColumnOrder(5);
+
+            builder.Property(x => x.UserId)
+                .IsRequired(true)
+                .HasColumnOrder(6);
+
+            //Foreign Key
+
+            builder.HasOne(x => x.User)
+                .WithOne(x => x.Address)
+                .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.Restrict);
+        }
+    }
+}
