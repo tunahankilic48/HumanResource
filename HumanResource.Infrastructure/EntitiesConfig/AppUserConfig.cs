dbf78e59b6a8692986502a65887370aa242156d9
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-29 12:08:20 +0000

    Updated AppUserConfig.cs

--- HumanResource.Infrastructure/EntitiesConfig/AppUserConfig.cs
+++ HumanResource.Infrastructure/EntitiesConfig/AppUserConfig.cs
@@ -13,8 +13,26 @@ namespace HumanResource.Infrastructure.EntitiesConfig
         public override void Configure(EntityTypeBuilder<AppUser> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.FirstName).IsRequired(true).HasMaxLength(30);
-            builder.Property(x => x.LastName).IsRequired(true).HasMaxLength(50);
+            builder.Property(x=>x.Id).HasColumnOrder(1);
+            builder.Property(x => x.FirstName).IsRequired(true).HasMaxLength(30).IsUnicode(true)..HasColumnOrder(2);
+            builder.Property(x => x.LastName).IsRequired(true).HasMaxLength(50).IsUnicode(true).HasColumnOrder(3);
+            builder.Property(x=>x.BirthDate).IsRequired(false).HasColumnType("date").HasColumnOrder(4);
+            builder.Property(x => x.AddressId)
+                .IsRequired(true).HasColumnOrder(5);
+            builder.Property(x => x.DepartmentId)
+                .IsRequired(true).HasColumnOrder(6);
+            builder.Property(x => x.BloodTypeId)
+                .IsRequired(true).HasColumnOrder(7);
+            builder.Property(x => x.ManagerId)
+                .IsRequired(true).HasColumnOrder(8);
+            
+            //Foreign Key ToDo: Kendi içinde eklenecek
+
+            //Foreign Key
+            builder.HasOne(x=>x.BloodType)
+                    .WithMany(x=>x.Users)
+                    .ForeignKey(x=>x.BloodTypeId);
+            
 
             base.Configure(builder);
         }
