67cabf1300347827d1c95095bd06df93a7d6a6d7
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-29 10:08:31 +0000

    Updated Address.cs

--- HumanResource.Domain/entities/Address.cs
+++ HumanResource.Domain/entities/Address.cs
@@ -8,14 +8,15 @@ namespace HumanResource.Domain.Entities
 {
     public class Address : IBaseEntity
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int PostCode { get; set; }
-        public int CityId { get; set; }
         public int DistrictId { get; set; }
+        public Guid AppUserId { get; set; }
 
         //navigation
-        public City City { get; set; }
         public District District { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
