af18701f8ea67fa80570dbbf28c055804b6b1e8c
ahmetmanco <ahmetmanco07@gmail.com>
2023-04-29 11:42:11 +0300

    AppUser added

--- HumanResource.Domain/entities/AppUser.cs
+++ HumanResource.Domain/entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace HumanResource.Domain.Entities
 {
-    internal class AppUser
+    public class AppUser : IdentityUser<Guid>
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string TelNo { get; set; }
+        public int addressId { get; set; }
+        public string departman { get; set; }
+        public DateTime WorkStartDate { get; set; }
+        public string adminFirstName { get; set; }
+        public string adminLastName { get; set; }
+
     }
 }
