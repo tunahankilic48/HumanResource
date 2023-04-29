4d8c8dc6c42a80037a30a4a6712ae8a87f662519
user <user@LAPTOP-5V57UFUH>
2023-04-29 12:36:59 +0300

    City was created

---
+++ HumanResource.Domain/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResource.Domain.Entities
+{
+	public class City
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+	}
+}
