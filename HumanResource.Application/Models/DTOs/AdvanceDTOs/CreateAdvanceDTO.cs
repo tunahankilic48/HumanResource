089d7aaf6b602acc8168b296b98d3277b75fbb79
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-30 13:59:38 +0000

    Updated CreateAdvanceDTO.cs

--- HumanResource.Application/Models/DTOs/AdvanceDTOs/CreateAdvanceDTO.cs
+++ HumanResource.Application/Models/DTOs/AdvanceDTOs/CreateAdvanceDTO.cs
@@ -11,8 +11,11 @@ namespace HumanResource.Application.Models.DTOs.AdvanceDTOs
 	public class CreateAdvanceDTO
 	{
 		[Required(ErrorMessage = "Miktar alanı boş geçilemez!")]
+		[Range(0,99999.99, ErrorMessage = "Lütfen 0-99999,99 arasında giriş yapınız.")]
 		public decimal Amount { get; set; }
+		
 		[Required(ErrorMessage = "Taksit alanı boş geçilemez!")]
+		[Range(0,10, ErrorMessage="Lütfen 0-10 arasında giriniz.")]
 		public int NumberOfInstallments { get; set; }
 		public DateTime CreateDate => DateTime.Now;
 		public Guid UserId { get; set; }
