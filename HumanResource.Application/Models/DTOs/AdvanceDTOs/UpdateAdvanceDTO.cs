649be6ffab13466a066666a10aec14d318daa21d
Tunahan Kılıç <tunahan.kilic@bilgeadamboost.com>
2023-04-30 14:00:37 +0000

    Updated UpdateAdvanceDTO.cs

--- HumanResource.Application/Models/DTOs/AdvanceDTOs/UpdateAdvanceDTO.cs
+++ HumanResource.Application/Models/DTOs/AdvanceDTOs/UpdateAdvanceDTO.cs
@@ -12,11 +12,15 @@ namespace HumanResource.Application.Models.DTOs.AdvanceDTOs
 	{
 		public int Id { get; set; }
 		[Required(ErrorMessage = "Miktar alanı boş geçilemez!")]
+		[Range(0,99999.99, ErrorMessage = "Lütfen 0-99999,99 arasında giriş yapınız.")]
 		public decimal Amount { get; set; }
+		
 		[Required(ErrorMessage = "Taksit alanı boş geçilemez!")]
+		[Range(0,10, ErrorMessage="Lütfen 0-10 arasında giriniz.")]
 		public int NumberOfInstallments { get; set; }
-		[Required(ErrorMessage = "Oluşturulma tarihi boş geçilemez!"), DataType(DataType.DateTime)]
+
 		public DateTime CreateDate { get; set; }
+		
 		[Required(ErrorMessage = "Güncelleme tarihi boş geçilemez!"), DataType(DataType.DateTime)]
 		public DateTime ModifiedDate { get; set; }
 		public Guid UserId { get; set; }
