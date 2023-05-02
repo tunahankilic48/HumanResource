f1301624fe4c4876ddb38e958686d15e4cf7ee1b
Tunahan Kilic <tunahankilic942@gmail.com>
2023-05-02 17:13:47 +0300

    Pop-ups were created for deletion, update and createion of leave. Leave controller were created.

---
+++ HumanRepository.Presentation/Areas/Personel/Controllers/LeaveController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanRepository.Presentation.Areas.Personel.Controllers
+{
+    [Authorize]
+    [Area("personel")]
+    public class LeaveController : Controller
+    {
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete()
+        {
+            return View();
+        }
+    }
+}
