8b36194d5c081a6c630d29ca77c6fb351c808e26
Tunahan Kilic <tunahankilic942@gmail.com>
2023-05-08 00:39:18 +0300

    .

--- HumanResource.Infrastructure/DbContext/ApplicationDbContext.cs
+++ HumanResource.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -3,12 +3,6 @@ using HumanResource.Infrastructure.EntitiesConfig;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HumanResource.Infrastructure.DbContext
 {
