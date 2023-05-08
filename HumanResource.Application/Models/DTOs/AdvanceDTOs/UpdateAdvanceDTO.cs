8920d1c40968738e6e1a43fbf02b531622d3eac5
Tunahan Kilic <tunahankilic942@gmail.com>
2023-05-08 16:56:54 +0300

    .

--- HumanResource.Application/Models/DTOs/AdvanceDTOs/UpdateAdvanceDTO.cs
+++ HumanResource.Application/Models/DTOs/AdvanceDTOs/UpdateAdvanceDTO.cs
@@ -15,10 +15,12 @@ namespace HumanResource.Application.Models.DTOs.AdvanceDTOs
         [Required(ErrorMessage = "Amount field cannot be empty!")]
         [Range(0, 99999.99, ErrorMessage = "Please enter between 0-99999.99!")]
         [RegularExpression(@"^\d+$", ErrorMessage = "No letters or symbols can be entered!")]
+        [Display(Name ="Amount")]
         public decimal Amount { get; set; }
 
         [Required(ErrorMessage = "Installment field cannot be empty!")]
         [Range(0, 10, ErrorMessage = "Please enter between 0-10.")]
+        [Display(Name ="Number Of Installments")]
         public int NumberOfInstallments { get; set; }
 
         public DateTime ModifiedDate => DateTime.Now;
