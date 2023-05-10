f360a3f2388082c3d4fe87f023cad071291c9b17
user <user@LAPTOP-5V57UFUH>
2023-05-10 20:52:37 +0300

    Company VM was created.

--- HumanResource.Application/Models/VMs/CompanyVM/CompanyVM.cs
+++ HumanResource.Application/Models/VMs/CompanyVM/CompanyVM.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HumanResource.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace HumanResource.Application.Models.VMs.CompanyVM
 {
     public class CompanyVM
     {
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Title")]
+        public Title Title { get; set; }
+
+        [Required(ErrorMessage = "Phone cannot be null.")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = ("Address Description"))]
+        public Address Address { get; set; }
+
+
+        [Display(Name = ("Company Name"))]
+        public Company Company { get; set; }
+
     }
 }
