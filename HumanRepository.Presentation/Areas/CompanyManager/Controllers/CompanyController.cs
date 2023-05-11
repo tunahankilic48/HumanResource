65be4781793d2ec1a583948dec02003b081e69fb
Tunahan Kilic <tunahankilic942@gmail.com>
2023-05-11 19:36:55 +0300

    Updated

---
+++ HumanRepository.Presentation/Areas/CompanyManager/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using HumanResource.Application.Services.PersonelService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResource.Presentation.Areas.CompanyManager.Controllers
+{
+    [Area("CompanyManager")]
+    [Authorize(Roles = "CompanyManager")]
+    public class CompanyController : Controller
+    {
+        private readonly IPersonelService _personelService;
+
+        public CompanyController(IPersonelService personelService)
+        {
+            _personelService = personelService;
+        }
+
+        public async Task<IActionResult> Company()
+        {
+            ViewBag.Personel = await _personelService.GetPersonel(User.Identity.Name);
+            return View();
+        }
+    }
+}
